7b9b4df4969e3c7d5b7b9ae49decee189d2cb24c
kfang9357 <43207499+kfang9357@users.noreply.github.com>
2019-04-01 19:11:38 -0400

    created MultPractice class :D

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,23 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice {
+    private int num1;
+    private int num2;
+
+    public MultPractice(int num1, int num2)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public String getProblem()
+    {
+        return (num1 + " TIMES "+ num2);
+    }
+
+    public void nextProblem()
+    {
+        num2++;
+    }
 }
